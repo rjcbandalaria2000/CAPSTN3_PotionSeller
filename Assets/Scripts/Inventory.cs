1b9970886e9bc8ac743ea2f92443b0471a3e31f8
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-07-08 21:05:04 +0800

    Created scripts for player inventory and shop

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Inventory : MonoBehaviour
+{
+    public List<GameObject> Items = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void AddToInventory(GameObject item)
+    {
+
+    }
+
+    public void RemoveToInventory(GameObject item)
+    {
+
+    }
+ 
+}
