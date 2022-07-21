6d401f732676d05693637964b08c61fa033d5a63
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-07-21 22:51:07 +0800

    Progress on Shop, Inventory, and Ingredients
    - Created ShopItemPanel for displaying the name and cost of the ingredient, and buy button
    - Created a Shop UI to display the items to sell
    - When buy button is pressed, it buys the item and subtracts the cost from the wallet if the money is enough

--- Assets/Scripts/UI/DisplayShopItem.cs
+++ Assets/Scripts/UI/DisplayShopItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ingredient : MonoBehaviour
+public class DisplayShopItem : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
