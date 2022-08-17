6c2abbfe305b02d18c9b231a13b03ff3a0e65183
Jace <jamesonandre.chiu@benilde.edu.ph>
2022-08-17 13:40:49 +0800

    Prototype Potion Ingredient Scriptable Object

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ingredient", menuName = "Potion Ingredient")]
+public class Ingredient : ScriptableObject
+{
+   public string ingredientName;
+    public int ID;  
+   public Sprite sprite;
+   
+    
+}
