2235710c47ba5670fb35f2e869306ca541d2d82c
Emman <emman.cardano@gmail.com>
2022-09-12 19:36:31 +0800

    Ingredients, Potions, and Pricing System WIP
    Items to check:
    > Created Scriptable Object scripts named 'Ingredient ScriptableObject.cs' and 'PotionScriptableObject.cs"
    > They both need a Name (string), Sprite, Buy Price (int), and Sell Price. (int?)
    > Check if they have Names and Buy Prices.
    > They don't have Sprites and/or Sell Prices yet.
    > 'PotionScriptableObject.cs' on the other hand needs type/s of 'IngredientScriptableObject' to consider recipe requirements.

---
+++ Assets/Scripts/Potions/PotionScriptableObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Potion Scriptable Object", menuName = "Scriptable Objects/Potion")]
+public class PotionScriptableObject : ScriptableObject
+{
+    public string potionName;
+    public Sprite potionSprite;
+    public float buyPrice;
+    public float sellPrice;
+
+    public List<IngredientScriptableObject> ingredients= new List<IngredientScriptableObject>();
+}
