771b66fc998a8ba64fd13ba1794d0dad7ffe4ce1
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-09-14 22:48:27 +0800

    Created scene and script for shop level

---
+++ Assets/StoreLevel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreLevel : MonoBehaviour
+{
+    public int Level;
+    public List<int> MaxExperience;
+    public int ExperiencePoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
