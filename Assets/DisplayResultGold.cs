c1440a3c5ce09185d67767cb6de3c3f6a7665b5c
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-10-24 21:09:03 +0800

    Added the goal and gold earned by the player in the result screen

---
+++ Assets/DisplayResultGold.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayResultGold : MonoBehaviour
+{
+    public Wallet playerWallet;
+    public TextMeshProUGUI resultGoldText; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        resultGoldText = this.GetComponent<TextMeshProUGUI>();
+        ShowPlayerGold();
+    }
+
+    public void ShowPlayerGold()
+    {
+        if(playerWallet == null) { return; }
+        resultGoldText.text = "Gold: " + playerWallet.Money.ToString("0");
+    }
+}
