648795437405773e9248cdb3b3cd00e57190ed43
Jace <jamesonandre.chiu@benilde.edu.ph>
2022-11-04 14:24:40 +0800

    Created Furniture Manager

---
+++ Assets/Scripts/Managers/FurnitureManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureManager : MonoBehaviour
+{
+    public List<GameObject> furnitures = new List<GameObject>();
+
+    public void Awake()
+    {
+        SingletonManager.Register(this);
+    }
+
+    public void ActivateFurniture(int storeLevel)
+    {
+        furnitures[storeLevel].SetActive(true);
+    }
+
+
+}
