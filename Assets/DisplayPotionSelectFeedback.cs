590e6a55a6b8789ebdbc66e4076ad766a25f6290
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-11-14 22:52:05 +0800

    Created text panel for showing feedback when selecting potion

---
+++ Assets/DisplayPotionSelectFeedback.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayPotionSelectFeedback : MonoBehaviour
+{
+    public CraftingManager craftingManager;
+    public TextMeshProUGUI feedbackText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
