fef4ce6c3dd0953edda012ae3beb1a38e22de63c
Jace <jamesonandre.chiu@benilde.edu.ph>
2022-11-17 11:46:09 +0800

    Add loadng screen

--- Assets/Scripts/Managers/BasicSceneManager.cs
+++ Assets/Scripts/Managers/BasicSceneManager.cs
@@ -6,6 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class BasicSceneManager : MonoBehaviour
 {
+    public GameObject loadingsScreen;
+    public Image loadingBar;
+
+    public void loadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        loadingsScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            loadingBar.fillAmount = progress;
+
+            yield return null;
+        }
+    }
+
+
    public void sceneLoad(string sceneName)
    {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
