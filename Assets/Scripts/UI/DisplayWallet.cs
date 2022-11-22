d4816a73e70e3c23e606c8ca6a322e3690fa19b6
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-11-22 21:43:17 +0800

    Refreshing the wallet count on start

--- Assets/Scripts/UI/DisplayWallet.cs
+++ Assets/Scripts/UI/DisplayWallet.cs
@@ -9,6 +9,12 @@ public class DisplayWallet : MonoBehaviour
     public GameObject Player;
     public TextMeshProUGUI MoneyCountText;
     private Wallet wallet;
+
+    private void Awake()
+    {
+        MoneyCountText = this.GetComponent<TextMeshProUGUI>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +27,7 @@ public class DisplayWallet : MonoBehaviour
         if (wallet)
         {
             wallet.WalletUIUpdate.AddListener(DisplayWalletCount);
+            DisplayWalletCount();
         }
 
     }
